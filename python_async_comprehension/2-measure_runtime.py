fd746bd0db3731fda58d71059ee1bd0b2b472250
ismael <ismaelbsd.pro@gmail>
2026-04-12 15:38:18 +0200

    python corriger

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python3
-"""Measure runtime"""
+"""Module measure_runtime"""
 
 import asyncio
 import time
-from typing import List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Execute async_comprehension 4 times in parallel"""
+    """Mesure le temps d'exécution"""
     start = time.perf_counter()
-
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-
-    end = time.perf_counter()
-    return end - start
+    return time.perf_counter() - start
