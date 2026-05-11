192c6258813e653cc114f9d17b45151101827506
ismael <ismaelbsd.pro@gmail>
2026-05-11 14:31:37 +0200

    node.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -5,7 +5,7 @@ function countStudents(path) {
 
   try {
     data = fs.readFileSync(path, 'utf8');
-  } catch (err) {
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
 
@@ -17,11 +17,11 @@ function countStudents(path) {
 
   const fields = {};
 
-  students.forEach((student) => {
-    const studentData = student.split(',');
+  students.forEach((line) => {
+    const parts = line.split(',');
 
-    const firstname = studentData[0];
-    const field = studentData[3];
+    const firstname = parts[0];
+    const field = parts[3];
 
     if (!fields[field]) {
       fields[field] = [];
@@ -30,9 +30,14 @@ function countStudents(path) {
     fields[field].push(firstname);
   });
 
-  for (const field in fields) {
+  const keys = Object.keys(fields);
+
+  for (let i = 0; i < keys.length; i += 1) {
+    const field = keys[i];
+    const list = fields[field];
+
     console.log(
-      `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+      `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`
     );
   }
 }
--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,27 +2,24 @@ const fs = require('fs');
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
         reject(new Error('Cannot load the database'));
         return;
       }
 
-      const lines = data
-        .split('\n')
-        .filter((line) => line.trim() !== '');
-
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
       const students = lines.slice(1);
 
       console.log(`Number of students: ${students.length}`);
 
       const fields = {};
 
-      students.forEach((student) => {
-        const studentData = student.split(',');
+      students.forEach((line) => {
+        const parts = line.split(',');
 
-        const firstname = studentData[0];
-        const field = studentData[3];
+        const firstname = parts[0];
+        const field = parts[3];
 
         if (!fields[field]) {
           fields[field] = [];
@@ -31,9 +28,14 @@ function countStudents(path) {
         fields[field].push(firstname);
       });
 
-      for (const field in fields) {
+      const keys = Object.keys(fields);
+
+      for (let i = 0; i < keys.length; i += 1) {
+        const field = keys[i];
+        const list = fields[field];
+
         console.log(
-          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+          `Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`
         );
       }
 
